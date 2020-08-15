8b537b3640aaf743b382d841563e5a6310ebf17a
Stas_SD <sidelnikov.si@phystech.edu>
2020-08-15 10:18:32 +0300

    commented storage.py

--- storage.py
+++ storage.py
@@ -3,6 +3,23 @@ import tempfile
 import json
 import argparse
 
+DEBUG = True
 
-storage_path = os.path.join(tempfile.gettempdir(), "storage.data")
+
+def print_path(storage_path):
+	print("Storaga path is: ", storage_path)
+
+
+def main():
+	storage_path = os.path.join(tempfile.gettempdir(), "storage.data") # not in the current directory
+
+	if DEBUG:
+		print_path(storage_path)
+
+
+
+
+main()
+
+
 #with open(storage_path, "w") as file:
